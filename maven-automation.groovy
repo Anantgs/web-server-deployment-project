6e5d2a47506acec5ce12e5fc1b91523eb9f01842
Anantgs <anantgsaraf123@gmail.com>
2024-01-21 13:08:31 +0530

    Jenkins file maven-automation.groovy added

---
+++ maven-automation.groovy
@@ -0,0 +1,38 @@
+pipeline {
+    agent any
+
+        environment {
+        DOCKER_IMAGE = 'registry.hub.docker.com/anantgsaraf/centos-aws-cli-image:1.0.1'
+        PYTHON_SCRIPT = 'ami-creation.py'
+    }
+
+
+    stages {
+
+        stage('Clone from GitLab') {
+            steps {
+                script {
+                    // Define GitLab repository and credentials
+                    def gitLabRepoUrl = 'https://gitlab.com/devops5113843/web-server-deployment-project.git'
+                    def gitLabCredentialsId = 'gitlab-credentials-id'
+
+                    // Clone GitLab repository
+                    withCredentials([usernamePassword(credentialsId: gitLabCredentialsId, usernameVariable: 'anantgsaraf123', passwordVariable: 'Kigo@2022')]) {
+                        git branch: 'main', url: gitLabRepoUrl, credentialsId: gitLabCredentialsId
+                    }
+
+                    sh "cd ${env.WORKSPACE} ; ls -l"
+
+                }
+            }
+        }
+
+    }
+
+    post {
+        always {
+            // Clean up
+            cleanWs()
+        }
+    }
+}
