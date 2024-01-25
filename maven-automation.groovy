ffdd2fad9a75f4b24b48fdfb51d5333637d862ca
Anantgs <anantgsaraf123@gmail.com>
2024-01-25 08:04:22 +0530

    Nexus server ip changed

--- maven-automation.groovy
+++ maven-automation.groovy
@@ -8,7 +8,7 @@ pipeline {
         MAVEN_HOME = tool 'Maven'
         DOCKER_IMAGE = 'web-server-example'
         DOCKER_REGISTRY_CREDENTIALS = credentials('docker-login')
-        NEXUS_REPO_URL = '3.84.159.105:8083'
+        NEXUS_REPO_URL = '54.152.98.14:8083'
         }
 
 
