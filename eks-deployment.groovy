82487a5325694e779c2c1529c608e76af9eba1ed
Anantgs <anantgsaraf123@gmail.com>
2024-01-31 23:36:03 +0530

    Project Completed

---
+++ eks-deployment.groovy
@@ -0,0 +1,87 @@
+pipeline {
+    parameters {
+        choice(name: 'ACTION', choices: ['create', 'delete'], description: 'Select the action to perform')
+    }
+    
+    agent {
+        label 'jenkins'
+    }
+
+    environment {
+        // Use Jenkins credentials for AWS
+        //def AWS_ACCESS_KEY_ID     = aws-access-key-id
+        //def AWS_SECRET_ACCESS_KEY = aws-secrete-key-id
+        AWS_DEFAULT_REGION    = 'us-east-1' // Replace with your AWS region
+    }
+
+    stages {
+        stage('Configure AWS') {
+            steps {
+                withCredentials([[
+                                    $class: 'AmazonWebServicesCredentialsBinding',
+                                    credentialsId: "AWS-CREDENTIALS",
+                                    accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                                    secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
+                                ]]){
+                                    
+                    // Your AWS-related steps here
+                    //sh 'aws s3 ls' // Example AWS CLI command
+                    echo "AWS_ACCESS_KEY_ID: ${AWS_ACCESS_KEY_ID}"
+                    echo "AWS_SECRET_ACCESS_KEY: ${AWS_SECRET_ACCESS_KEY}"                    
+                }
+            }
+        }
+
+        // stage('Checkout') {
+        //     steps {
+        //         script {
+        //             // Clone your GitLab repository using global credentials
+        //             git credentialsId: 'gitlab-credentials-id', url: 'https://gitlab.com/devops5113843/terraform-eks.git', clean: true
+        //         }
+        //     }
+        // }
+
+        stage('Terraform Init') {
+            steps {
+                script {
+                    // Run Terraform init
+                  sh 'ls -l'
+                  dir('Kubernetes-deployments') {  
+                    sh 'ls -l'
+                    sh 'aws eks update-kubeconfig --region us-east-1 --name my-eks-cluster'
+                    sh 'kubectl create ns test'
+                    sh 'kubectl create secret docker-registry ecr-secret \
+                        --docker-server=ecr-repo-url \
+                        --docker-username=AWS \
+                        --docker-password="$(aws ecr get-login-password --region us-east-1)" \
+                        --docker-email=none@example.com -n test'
+                  } 
+                }
+            }
+        }
+
+        stage('Terraform Action') {
+            steps {
+                script {
+                    dir('Kubernetes-deployments') {  
+                            // Determine the Terraform action based on the selected parameter
+                            def kubeAction = params.ACTION == 'create' ? 'create' : 'delete'
+
+                            // Run Terraform apply or destroy
+                            sh "kubectl ${kubeAction} -f service.yaml"
+                            sh "kubectl ${kubeAction} -f deployment.yaml"                            
+                    }    
+                }
+            }
+        }
+    }
+
+    post {
+        success {
+            echo 'Terraform deployment successful!'
+        }
+        failure {
+            echo 'Terraform deployment failed!'
+        }
+    }
+}
